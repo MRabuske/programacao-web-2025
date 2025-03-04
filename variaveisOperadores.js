7f0dd7b96d9a04630b6205865f0d508e6a983c51
MRabuske <matheusrabuske@hotmail.com>
2025-03-04 14:03:15 -0300

    Create variaveisOperadores.js

---
+++ variaveisOperadores.js
@@ -0,0 +1,23 @@
+// Declaração das variáveis
+let numeroA = 13;
+let numeroB = 7;
+
+// Operação de adição
+let soma = numeroA + numeroB;
+console.log(`Resultado da adição entre ${numeroA} e ${numeroB} é ${soma}`);
+
+// Operação de subtração
+let subtracao = numeroA - numeroB;
+console.log(`Resultado da subtração entre ${numeroA} e ${numeroB} é ${subtracao}`);
+
+// Operação de multiplicação
+let multiplicacao = numeroA * numeroB;
+console.log(`Resultado da multiplicação entre ${numeroA} e ${numeroB} é ${multiplicacao}`);
+
+// Operação de divisão
+let divisao = numeroA / numeroB;
+console.log(`Resultado da divisão entre ${numeroA} e ${numeroB} é ${divisao}`);
+
+// Operação módulo (resto)
+let modulo = numeroA % numeroB;
+console.log(`Resultado do módulo entre ${numeroA} e ${numeroB} é ${modulo}`);
